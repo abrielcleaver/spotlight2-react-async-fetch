c5e28c01dfeed7795127d212050450f5dc0b8ee5
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-04 15:42:42 -0700

    Import DogsList component and getDogs function in App.js. added useEffect callback to fetch dogs from supabase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+// Import the fetch function into App.js
+import { getDogs } from './services/fetch-utils';
 // import your arrays here
+import DogsList from './DogsList';
+
 
 function App() {
+  const [dogs, setDogs] = useState([]);
+
+  async function fetchAndStoreDogs() {
+    const data = await getDogs();
+    setDogs(data);
+// //useEffect is a hook that gets called on every change
+//   // If the array is empty, useEffect is called once on load
+//   // if something is in the array, the useEffect callback gets called whenever what's inside the array changes.
+  }
+  useEffect(() => {
+    fetchAndStoreDogs();
+  }, []);
+
   return (
     <div className="App">
-        Render all your lists here. Pass the arrays as props.
+      <DogsList doggies={dogs}/>
     </div>
+    
   );
 }
 
