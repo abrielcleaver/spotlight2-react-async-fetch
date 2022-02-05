86809257ee969ed9dba992c3da2318894dbda109
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-04 17:21:20 -0700

    Update formatting in DogsList

--- src/DogsList.js
+++ src/DogsList.js
@@ -4,6 +4,9 @@ import DogItem from './DogItem';
 export default function DogsList({ doggies }) {
   return <div>
     <h2>Welcome to the DogsList</h2>
-    {doggies.map((dog, i) => <DogItem dog={dog} key={`dog}-${i}`}/>)}
+    {doggies.map((dog, i) => 
+      <DogItem 
+        dog={dog} 
+        key={`dog}-${i}`}/>)}
   </div>;
 }
